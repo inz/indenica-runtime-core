08ce41f441ee8470899f5604b28ef9dc7b20f3e3
Christian Inzinger <inzinger@dsg.tuwien.ac.at>
2012-09-13 11:58:51 +0200

    Add JAXB annotation to ActionEvent.

--- src/main/java/eu/indenica/events/ActionEvent.java
+++ src/main/java/eu/indenica/events/ActionEvent.java
@@ -3,12 +3,15 @@
  */
 package eu.indenica.events;
 
+import javax.xml.bind.annotation.XmlRootElement;
+
 import eu.indenica.adaptation.Action;
 
 /**
  * @author Christian Inzinger
- *
+ * 
  */
+@XmlRootElement
 public class ActionEvent extends Event {
 	public static final String ACTION_EVENT_TYPE = "action";
 	private final Action action;
@@ -20,7 +23,14 @@ public class ActionEvent extends Event {
 		super(ACTION_EVENT_TYPE);
 		this.action = action;
 	}
-	
+
+	/**
+	 * 
+	 */
+	public ActionEvent() {
+		action = null;
+	}
+
 	/**
 	 * @return the action
 	 */
