84ff92efb88ad2d9adfa4114ef39746f4849768e
Christian Inzinger <inzinger@dsg.tuwien.ac.at>
2012-11-06 16:37:43 +0100

    Add test Event

---
+++ src/test/java/testPkg/EventOne.java
@@ -0,0 +1,26 @@
+package testPkg;
+
+// + XMLRootElement!
+public class EventOne extends eu.indenica.events.Event {
+	public EventOne() {
+		super("testPkg.EventOne");
+	}
+	private String attr1;
+	
+	public String getAttr1() {
+		return attr1;
+	}
+	
+	public void setAttr1(final String attr1) {
+		this.attr1 = attr1;
+	}
+	private int anAttribute;
+	
+	public int getAnAttribute() {
+		return anAttribute;
+	}
+	
+	public void setAnAttribute(final int anAttribute) {
+		this.anAttribute = anAttribute;
+	}
+}
