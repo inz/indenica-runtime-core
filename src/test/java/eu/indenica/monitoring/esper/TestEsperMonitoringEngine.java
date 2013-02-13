cc83c8e5f134314334cab340e62cf1a2bcfa63ef
Christian Inzinger <inzinger@dsg.tuwien.ac.at>
2013-02-13 23:31:43 +0100

    Fix test breakage.

--- src/test/java/eu/indenica/monitoring/esper/TestEsperMonitoringEngine.java
+++ src/test/java/eu/indenica/monitoring/esper/TestEsperMonitoringEngine.java
@@ -5,8 +5,9 @@ package eu.indenica.monitoring.esper;
 
 import static eu.indenica.common.TestUtils.createEventListener;
 import static eu.indenica.common.TestUtils.setLogLevels;
-import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 import java.util.Collection;
 import java.util.concurrent.Semaphore;
@@ -20,13 +21,15 @@ import org.slf4j.Logger;
 
 import com.google.common.collect.Lists;
 
+import eu.indenica.common.ActivemqPubSub;
 import eu.indenica.common.LoggerFactory;
-import eu.indenica.common.PubSubFactory;
+import eu.indenica.common.PubSub;
 import eu.indenica.events.Event;
 import eu.indenica.events.EventOne;
 import eu.indenica.messaging.MessageBroker;
 import eu.indenica.messaging.NameProvider;
 import eu.indenica.monitoring.MonitoringEngine;
+import eu.indenica.monitoring.MonitoringQuery;
 import eu.indenica.monitoring.MonitoringQueryImpl;
 
 /**
@@ -38,6 +41,7 @@ public class TestEsperMonitoringEngine {
 	private static Logger LOG = LoggerFactory.getLogger();
 	private Semaphore msgWaitLock;
 	private NameProvider nameProvider;
+	private PubSub pubSub;
 	private MessageBroker broker;
 	private MonitoringEngine monitoringEngine;
 
@@ -51,6 +55,7 @@ public class TestEsperMonitoringEngine {
 		msgWaitLock = new Semaphore(0);
 		nameProvider = new NameProvider("test-" + System.currentTimeMillis());
 		broker = new MessageBroker(nameProvider);
+		pubSub = new ActivemqPubSub();
 		monitoringEngine = new EsperMonitoringEngine();
 		monitoringEngine.init();
 	}
@@ -59,6 +64,7 @@ public class TestEsperMonitoringEngine {
 	public void tearDown() throws Exception {
 		msgWaitLock.drainPermits();
 		monitoringEngine.destroy();
+		pubSub.destroy();
 		broker.destroy();
 	}
 
@@ -81,12 +87,12 @@ public class TestEsperMonitoringEngine {
 		monitoringEngine.addQuery(query);
 
 		Collection<Event> observedEvents = Lists.newArrayList();
-		createEventListener(PubSubFactory.getPubSub(),
-				new EventOne().getEventType(), observedEvents, msgWaitLock);
+		createEventListener(pubSub, new EventOne().getEventType(),
+				observedEvents, msgWaitLock);
 
 		EventOne event = new EventOne();
 		event.setMessage("message " + System.currentTimeMillis());
-		PubSubFactory.getPubSub().publish("input", event);
+		pubSub.publish("input", event);
 
 		assertThat(msgWaitLock.tryAcquire(2, TimeUnit.SECONDS), is(true));
 		assertThat(observedEvents, hasItem((Event) event));
